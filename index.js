1df2ae76e221f78bc02decfe73e78989c07c989b
Blindeen <kuba.szmajda@gmail.com>
2025-07-11 08:53:19 +0200

    install and config cors

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
